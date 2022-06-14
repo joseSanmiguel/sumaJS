4e7bfb3b8a8ca249384f9f23b868c7dca7fb93d4
Jose-sanmiguel <86948429+Jose-sanmiguel@users.noreply.github.com>
2022-06-15 10:10:36 +1200

    html y javascript estan funcionales.
    No se coloco ninguna informacion en css

---
+++ suma/suma.js
@@ -0,0 +1,6 @@
+function sum(){
+    var val1 = document.getElementById("valor1").value;
+    var val2 = document.getElementById("valor2").value;
+    var result =  parseInt(val1) + parseInt(val2)
+    var resultado = document.getElementById("Resultado").innerHTML = result
+}
